cc973148b4602339db360b82eb47c037bc13ee8a
andrit91 <73693163+andrit91@users.noreply.github.com>
2021-03-02 17:02:34 +0100

    Fir filter: single tap module and generic fir

---
+++ genericfir.sv
@@ -0,0 +1,53 @@
+`default_nettype	none
+//hello
+module	genericfir(i_clk, i_ce, i_sample, o_result);
+	parameter		NTAPS=5, IW=12, TW=IW, OW=2*IW+7;
+	input	wire			i_clk;
+	//
+	//
+	input	wire			i_ce;
+	input	wire	[(IW-1):0]	i_sample;
+	output	wire	[(OW-1):0]	o_result;
+
+	reg    	[(TW-1):0] tap		[NTAPS:0];
+	wire	[(TW-1):0] tapout	[NTAPS:0];
+	wire	[(IW-1):0] sample	[NTAPS:0];
+	wire	[(OW-1):0] result	[NTAPS:0];
+	wire		tap_wr; //delete it?
+
+	// The first sample in our sample chain is the sample we are given
+	assign	sample[0]	= i_sample;
+	// Initialize the partial summing accumulator with zero
+	assign	result[0]	= 0;
+
+	genvar	k;
+	generate
+	
+	begin
+		initial $readmemh("taps.txt", tap);
+
+		assign	tap_wr = 1'b0;
+	end 
+
+	for(k=0; k<NTAPS; k=k+1)
+	begin: FILTER
+
+		firtap #(.IW(IW), .OW(OW), .TW(TW)
+				)
+			tapk(i_clk, 
+				// Tap update circuitry
+				 tap[NTAPS-1-k], tapout[k],
+				// Sample delay line
+				i_ce, sample[k], sample[k+1],
+				// The output accumulator
+				result[k], result[k+1]);
+
+
+
+	end endgenerate
+
+	assign	o_result = result[NTAPS];
+
+
+
+endmodule
---
+++ tbfir.sv
@@ -0,0 +1,43 @@
+module tbfir;
+
+//inputs
+
+reg Clk;
+reg signed [11:0] Xin;
+reg Ice;
+
+// Outputs
+wire signed [30:0] Yout;
+
+
+    // Instantiate the Unit Under Test (UUT)
+    genericfir uut (
+        .i_clk(Clk),
+        .i_ce(Ice), 
+        .i_sample(Xin), 
+        .o_result(Yout)
+    );
+
+//Generate a clock with 10 ns clock period.
+initial 
+    begin
+        Clk = 0;
+        Ice = 1;
+    end
+always #5 Clk =~Clk;
+
+//Initialize and apply the inputs.
+    initial begin
+          Xin = 0;  #40;
+          Xin = 1; #10;
+          Xin = 0;  #10;
+          Xin = 0;  #10;
+          Xin = 0; #10;
+          Xin = 0; #10;
+          Xin = 0;  #10;
+          Xin = 0; #10;
+          Xin = 0;  #10;
+          Xin = 0;  #10;
+    end
+
+endmodule
